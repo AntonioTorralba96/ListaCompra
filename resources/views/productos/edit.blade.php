ce3f4f2e64ee584287fce1c17b6df52e7a016bdc
AntonioTorralba <atorralba96@gmail.com>
2019-02-07 09:22:24 +0100

    controladores

--- resources/views/productos/edit.blade.php
+++ resources/views/productos/edit.blade.php
@@ -2,6 +2,48 @@
 
 @section('content')
 
-    Modificar producto
+    <div class="row" style="margin-top:40px">
+        <div class="offset-md-3 col-md-6">
+            <div class="card">
+                <div class="card-header text-center">
+                    Modificar Producto
+                </div>
+                <div class="card-body" style="padding:30px">
+
+                    {{-- TODO: Abrir el formulario e indicar el método POST --}}
+                    <form method="post" action="javascript:void(0)">
+                        {{method_field('PUT')}}
+                    {{-- TODO: Protección contra CSRF --}}
+                        @csrf
+                    <div class="form-group">
+                        <label for="title">Nombre</label>
+                        <input type="text" name="nombre" id="title" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="title">Precio</label>
+                        <input type="number" name="precio" id="title" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="title">Categoría</label>
+                        <input type="text" name="categoria" id="title" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="title">Imagen</label>
+                        <input type="text" name="imagen" id="title" class="form-control">
+                    </div>
+
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                            Modificar producto
+                        </button>
+                    </div>
+
+                    {{-- TODO: Cerrar formulario --}}
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
 
 @stop
--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 
-    Vista detalles productos
+    <div class="row">
+
+        <div class="col-sm-4">
+
+            {{-- TODO: Imagen genérica de los productos --}}
+            <img src="https://picsum.photos/200/300/?random" class="img-fluid"/><br><br>
+
+            <button type="button" class="btn btn-dark"><i class="fas fa-arrow-left"></i>Insertar Imagen</button>
+
+        </div>
+        <div class="col-sm-8">
+
+            {{-- TODO: Datos del producto --}}
+            <p><b>Producto:</b>{{$productos[0]}}</p>
+            <p><b>Categoria:</b>{{$productos[1]}}</p>
+
+            <p>Estado: Producto actualmente comprado</p>
+            <a class="btn btn-danger" role="button">Pendiente de compra</a>
+        </div>
+    </div>
 
 @stop
